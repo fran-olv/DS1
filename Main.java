1e8712e8526e48c615d8c131630d53dc5e579d79
Franciele B. de Oliveira <>
2021-05-01 01:09:49 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+class Main {
+  public static void main(String[] args) {
+    String login, senha;
+
+  login = JOptionPane.showInputDialog("Entre com o Login"); senha = JOptionPane.showInputDialog("Entre com a senha"); 
+
+  if( login.equals("aluno") && senha.equals("aluno")) { 
+   JOptionPane.showMessageDialog(null, "Acesso liberado"); 
+  }else { 
+     JOptionPane.showMessageDialog(null, "Login ou senha incorretos"); 
+     } // fim do if }//fim do main } // fim da classe 
+  }
+}
